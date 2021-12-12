07b2eca7f0e801fddc3ba0a96a9fffcc7af74f7e
cornprincess <xiaocorn96@gmail.com>
2021-12-13 00:02:58 +0800

    add ch12

---
+++ ch12/stringbad.h
@@ -0,0 +1,30 @@
+//
+// Created by 周天斌 on 2021/12/12.
+//
+#include <iostream>
+#ifndef CPP_PRACTICE_STRINGBAD_H
+#define CPP_PRACTICE_STRINGBAD_H
+
+class StringBad{
+private:
+    // pointer to the string
+    char * str;
+    // length of string
+    int len;
+    // number of objects
+    static int num_strings;
+public:
+    // default constructor
+    StringBad();
+    // constructor
+    StringBad(const char * s);
+    // destructor
+    ~StringBad();
+    char* getStr() {
+        return str;
+    }
+    // friend function
+    friend std::ostream & operator<<(std::ostream & os, const StringBad &st);
+};
+
+#endif //CPP_PRACTICE_STRINGBAD_H
