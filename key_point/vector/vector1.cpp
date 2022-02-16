373b48bccd468037758b4cf8aee9a226cb6540f1
cornprincess <xiaocorn96@gmail.com>
2022-02-16 21:58:42 +0800

    add vector practice code

--- key_point/vector/vector1.cpp
+++ key_point/vector/vector1.cpp
@@ -2,3 +2,16 @@
 // Created by 周天斌 on 2022/2/16.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v1 = {1,2};
+    vector<vector<int>> v2 = {{}};
+    cout << v1.size() << endl;
+    cout << v2.size() << endl;
+    cout << v2[0].size() << endl;
+    return 0;
+}
